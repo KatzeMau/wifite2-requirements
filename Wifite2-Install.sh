9e6f556a340fb7cdda5543a2ef109f397e914d6d
Katze_Mau <46974624+KatzeMau@users.noreply.github.com>
2020-04-21 23:07:28 +0200

    Add files via upload

---
+++ Wifite2-Install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+apt-get update 
+apt-get upgrade -y
+apt install git -y 
+git clone https://github.com/derv82/wifite2.git
+cd wifite2
+sudo python setup.py install
+cd
+git clone https://github.com/ZerBea/hcxdumptool
+git clone https://github.com/ZerBea/hcxtools
+apt install libssl-dev -y 
+cd hcxdumptool 
+make 
+make install 
+cd 
+cd hcxtools 
+apt install libz-dev -y 
+apt install libcurl4-gnutls-dev -y 
+make 
+make install
+cd
+apt-get install bully -y
+git clone https://github.com/JPaulMora/Pyrit
+apt-get install python -y
+sudo apt-get install python3-dev -y
+sudo apt-get install python2-dev -y
+cd Pyrit
+apt-get install libpcap-dev -y
+python setup.py clean 
+python setup.py build 
+python setup.py install
+cd 
+apt-get install wireshark -y
+apt-get install wireless-tools -y
+apt-get install macchanger -y
+exit
