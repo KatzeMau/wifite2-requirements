c86c109b626289e61d406ad1f39a49868a9f1c5c
Katze_Mau <46974624+KatzeMau@users.noreply.github.com>
2020-05-24 11:20:19 +0200

    Script For Wifite Mod
    Here is the install script for the wifite2 mod install script

---
+++ Wifite2-Mod-By-nuncan.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+apt-get update 
+apt-get upgrade -y
+apt install git -y 
+dpkg --configure -a
+git clone https://github.com/nuncan/wifite2mod
+cd wifite2
+git pull
+sudo python3 setup.py
+cd
+git clone https://github.com/ZerBea/hcxdumptool
+git clone https://github.com/ZerBea/hcxtools
+apt install libssl-dev -y 
+cd hcxdumptool 
+git pull
+make 
+make install 
+cd 
+cd hcxtools 
+git pull
+apt install libz-dev -y 
+apt install libcurl4-gnutls-dev -y 
+make 
+make install
+cd
+apt-get install bully -y
+git clone https://github.com/JPaulMora/Pyrit
+apt-get install python -y
+sudo apt-get install python3-dev -y
+sudo apt-get install python2-dev -y
+cd Pyrit
+git pull
+apt-get install libpcap-dev -y
+python setup.py clean 
+python setup.py build 
+python setup.py install
+cd 
+apt-get install wireshark -y
+apt-get install wireless-tools -y
+apt-get install macchanger -y
+exit
